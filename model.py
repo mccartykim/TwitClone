0ec498f31d52e40cf2464573bd1070ff98b4d919
mccartytim <mccartyt1@montclair.edu>
2015-09-14 01:03:19 -0400

    Passed the first set of tests for the model class
    This gives me a base to build on for a more robust and useful model class

--- model.py
+++ model.py
@@ -1,14 +1,19 @@
 import sqlite3
+import uuid
 
 class Model:
     def __init__(self, dburl):
         self.con = sqlite3.connect(dburl)
 
     def setup(self):
-        cur = self.con.cursor()
-        cur.execute("CREATE TABLE users (uid blob PRIMARY KEY, username varchar(28))")
-        cur.execute("CREATE TABLE posts (pid blob PRIMARY KEY, uid blob, message text, postedAt integer DEFAULT CURRENT_TIMESTAMP, FOREIGN KEY (uid) REFERENCES users(uid))")
-        self.con.commit()
+        #cur = self.con.cursor()
+        with self.con:
+            self.con.execute("DROP TABLE IF EXISTS users")
+            self.con.execute("DROP TABLE IF EXISTS posts")
+            self.con.execute("CREATE TABLE users (uid blob PRIMARY KEY, username varchar(28) UNIQUE)")
+            self.con.execute("CREATE TABLE posts (pid blob PRIMARY KEY, uid blob, message text, postedAt integer DEFAULT CURRENT_TIMESTAMP, FOREIGN KEY (uid) REFERENCES users(uid))")
+        #self.con.commit()
+        #self.con.close()
         return True
     
     def close(self):
@@ -17,23 +22,51 @@ class Model:
     def addUser(self, username):
         #TODO: Validate user input
         #TODO: raise and handle exceptions in the event of invalid input or sql glitch
-        userID = 0
+        userID = uuid.uuid4()
+        try:
+            cur = self.con.cursor()
+            cur.execute("INSERT INTO users VALUES (?, ?)", (sqlite3.Binary(userID.bytes), username))
+            self.con.commit()
+        except sqlite3.IntegrityError:
+            self.con.rollback()
+            return None
         return userID
 
     def addPost(self, userID, message):
         #should this take a session ID?
         #post to the user's stream a new message
-        postID = ""
-        return postID
-
-    def getUser(self, userID):
-        return ""
-
-    def getPost(self, postID):
+        if len(message) == 0 or len(message) > 140:
+            return None
+        postID = uuid.uuid4()
+        with self.con:
+            self.con.execute("INSERT INTO posts(pid, uid, message) VALUES (?, ?, ?)", (sqlite3.Binary(postID.bytes), sqlite3.Binary(userID.bytes), message))
+            return postID
         return None
 
-    def getUserPosts(self, userID, start=0):
-        return []
+    def getUser(self, userID):
+        cur = self.con.cursor()
+        cur.execute("SELECT username FROM users WHERE uid == ?", (sqlite3.Binary(userID.bytes),))
+        username = cur.fetchone()[0];
+        return username
+
+    def getPost(self, postID):
+        cur = self.con.cursor()
+        cur.execute("SELECT message FROM posts where pid == ?", (sqlite3.Binary(postID.bytes),))
+        return cur.fetchone()[0]
+
+    def getUserPosts(self, userID, start=0, sel_length=10):
+        cur = self.con.cursor()
+        cur.execute("""SELECT users.username, posts.message, posts.postedAt, posts.pid
+                   FROM users
+                   JOIN posts using (uid)
+                   WHERE uid == ?
+                   ORDER BY posts.rowid DESC LIMIT ?""", (sqlite3.Binary(userID.bytes), sel_length))
+        rows = cur.fetchall()
+        results = []
+        for entry in rows:
+            post = {"username": entry[0], "message": entry[1], "postedAt": entry[2], "postID": entry[3]}
+            results.append(post)
+        return results
 
     def deletePost(self, messageID):
         return None
--- tests/tests.py
+++ tests/tests.py
@@ -1,8 +1,24 @@
 import unittest
+import uuid
 from model import Model
 
-mockModel = Model(":memory:")
-mockUID = None
+#mockModel = Model("test.db")
+mockUID = uuid.uuid4()
+
+class MockModel:
+    def createModel():
+        mock = Model("test.db")
+        return mock
+
+    def setup():
+        mock = MockModel.createModel()
+        mock.setup()
+        return mock
+
+    def createUser():
+        mock = MockModel.setup()
+        uid = mock.addUser("foo bar")
+        return (mock, uid)
 
 class ModelTest(unittest.TestCase):
     
@@ -11,28 +27,31 @@ class ModelTest(unittest.TestCase):
         pass
 
     def tearDown(self):
-        #mockModel.close()
-        #del mockModel()
+        #mock.close()
         pass
 
     def test0ModelInit(self):
-        mockModel = Model(":memory:")
-        result = mockModel.setup()
-        self.assertTrue(result, "Should return true on successful db creation")
+        #mockModel = Model(":memory:")
+        result = MockModel.createModel().setup()
+        self.assertIsNotNone(result, "Problem with successful db creation")
 
     def test0z(self):
-        mockUID = mockModel.addUser("foo bar")
-        self.assertEqual("foo bar", mockModel.getUser(mockUID), "Calling a user ID should return that user")
+        mock = MockModel.setup()
+        mockUID = mock.addUser("foo bar")
+        self.assertEqual("foo bar", mock.getUser(mockUID), "Calling a user ID should return that user")
 
     def test1(self):
-        uidBad = mockModel.addUser("foo bar")
+        mock = MockModel.setup()
+        for i in range(2):
+            uidBad = mock.addUser("foo bar")
         self.assertIsNone(uidBad, "Adding a user twice should fail and return a uid of none")
         
     def test2(self):
+        mock, uid = MockModel.createUser()
         for num in range(10):
-            mockModel.addPost(mockUID, str(num))
+            mock.addPost(uid, str(num))
             
-        posts = mockModel.getUserPosts(mockUID, 0)
+        posts = mock.getUserPosts(uid, 0)
 
         self.assertIsInstance(posts, list, "getUserPosts should return a list of posts")
 
@@ -40,16 +59,19 @@ class ModelTest(unittest.TestCase):
             self.assertEqual(posts[num]["message"], str(9-num), "Messages should be returned in reverse-chronological order")
 
     def test5(self):
-        pid = mockModel.addPost(mockUID, "x" * 140)
-        self.assertEqual(mockModel.getPost(pid), "x" * 140, "Should accept posts up to 140 characters")
+        mock, uid = MockModel.createUser()
+        pid = mock.addPost(uid, "x" * 140)
+        self.assertEqual(mock.getPost(pid), "x" * 140, "Should accept posts up to 140 characters")
 
     def test6(self):
-        pid2 = mockModel.addPost(mockUID, "")
-        self.assertIsNone(pid2, "Should reject 0 character messages.")
+        mock, uid = MockModel.createUser()
+        pid = mock.addPost(uid, "")
+        self.assertIsNone(pid, "Should reject 0 character messages.")
 
     def test7(self):
-        pid2 = mockModel.addPost(mockUID, "x" * 141)
-        self.assertIsNone(pid2, "Should reject messages with over 140 characters")
+        mock, uid = MockModel.createUser()
+        pid = mock.addPost(uid, "x" * 141)
+        self.assertIsNone(pid, "Should reject messages with over 140 characters")
 
 if __name__ == "__main__":
     unittest.main()
